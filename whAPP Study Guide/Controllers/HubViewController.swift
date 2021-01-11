aaec64eb70953b9cdb27a3c2d953a1d84b1eb7be
Nick Morris <nick88morris22@gmail.com>
2021-01-10 20:07:22 -0600

    added log out button

--- whAPP Study Guide/Controllers/HubViewController.swift
+++ whAPP Study Guide/Controllers/HubViewController.swift
@@ -6,15 +6,26 @@
 //
 
 import UIKit
+import Firebase
 
 class HubViewController: UIViewController {
     
 
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+    let firebaseAuth = Auth.auth()
+    do {
+      try firebaseAuth.signOut()
+        navigationController?.popToRootViewController(animated: true)
+    } catch let signOutError as NSError {
+      print ("Error signing out: %@", signOutError)
+    }
+    
+    }
     var count = 0.0
     var stop = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.hidesBackButton = true
         
         
     }
