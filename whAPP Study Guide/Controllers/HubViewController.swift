bc147a69e9d8e3796eb4b33e22b1fe3f96757429
Nick Morris <nick88morris22@gmail.com>
2021-01-13 09:33:00 -0600

    updated home screen

--- whAPP Study Guide/Controllers/HubViewController.swift
+++ whAPP Study Guide/Controllers/HubViewController.swift
@@ -10,7 +10,10 @@ import Firebase
 
 class HubViewController: UIViewController {
     
-
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var totalScoreLabel: UILabel!
+    @IBOutlet weak var averageScoreLabel: UILabel!
+    
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
     let firebaseAuth = Auth.auth()
     do {
@@ -23,11 +26,12 @@ class HubViewController: UIViewController {
     }
     var count = 0.0
     var stop = 0
+    let db = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
-        
-        
+        //load total score
+        loadGlobalQuestionCount()
     }
         /*    // MARK: - Navigation
      
@@ -37,5 +41,31 @@ class HubViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
-    
+    func loadGlobalQuestionCount(){
+        var max = 0
+        db.collection("scoreForDataBase").addSnapshotListener { (querySnapshot, error) in
+            if let e = error {
+                print("there was an issue retrieving data from Firestore \(e)")
+            } else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let chef = data["body"] as? Int, let userID = data["sender"] as? String {
+                            //print(chef)
+                            //print(userEmail)
+                            let currentID = Auth.auth().currentUser?.email
+                            let isEqual = (userID == currentID)
+                            if isEqual == true {
+                                if chef > max {
+                                    max = chef
+                                }
+                            }
+                        }
+                        
+                    }
+                    self.totalScoreLabel.text = String(max)
+                }
+            }
+        }
+    }
 }
