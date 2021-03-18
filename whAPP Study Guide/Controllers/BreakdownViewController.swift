be545cffea0a222a8fbb8ee4e7e3491869ee2f62
Nick22morris <nick88morris22@gmail.com>
2021-03-17 18:12:54 -0600

    tried to hide bar

--- whAPP Study Guide/Controllers/BreakdownViewController.swift
+++ whAPP Study Guide/Controllers/BreakdownViewController.swift
@@ -37,14 +37,12 @@ class BreakdownViewController: UIViewController {
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "doneToHome" {
+            let destinationVC = segue.destination as! ViewController
+            destinationVC.navigationController!.isNavigationBarHidden = true
+        }
     }
-    */
 
 }
